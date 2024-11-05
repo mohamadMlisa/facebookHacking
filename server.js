b90129e68ba6e8cc38d95d6fc73d1e295277d1cf
mohamadMlisa <mohamadmlisan2002@gmail.com>
2024-11-05 12:16:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 app.post("/submit", (req, res) => {
   const { email, password } = req.body;
   const filePath = path.join(__dirname, "data.txt");
